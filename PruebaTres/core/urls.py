2d969e1644f2efb56a2557aeb006f7728777f1c7
Sebastian Miranda <SebaMSH>
2021-06-23 02:19:00 -0400

    CRUD

--- PruebaTres/core/urls.py
+++ PruebaTres/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index , dulce, salada, mixta, contacto, nosotros, sismos, home, ingreso_proveedor, ingreso_pais
+from .views import index , dulce, salada, mixta, contacto, nosotros, sismos, home, ingreso_proveedor, ingreso_pais, listar_proveedor, form_mod_proveedor, form_del_proveedor
 
 
 urlpatterns = [
@@ -13,4 +13,7 @@ urlpatterns = [
     path('home', home, name="home"),
     path('ingreso_proveedor', ingreso_proveedor, name="ingreso_proveedor"),
     path('ingreso_pais', ingreso_pais, name="ingreso_pais"),
+    path('listar_proveedor', listar_proveedor, name="listar_proveedor"),
+    path('form_mod_proveedor/<id>', form_mod_proveedor, name="form_mod_proveedor"),
+    path('form_del_proveedor/<id>', form_del_proveedor, name="form_del_proveedor"),
 ]
--- PruebaTres/core/views.py
+++ PruebaTres/core/views.py
@@ -1,5 +1,6 @@
 from core.forms import ProveedoresForm, PaisForm
 from django.shortcuts import render,redirect
+from .models import Proveedores
 
 # Create your views here.
 def index(request):
@@ -49,4 +50,36 @@ def ingreso_pais(request):
             formulario.save()
             datos['mensaje'] = "Pais ingresado correctamente"
     return render(request, 'core/ingreso_pais.html',datos)
+
+def listar_proveedor(request):
+    Proveedor = Proveedores.objects.all()
+    datos = {
+        "Proveedor" : Proveedor
+    }
+    return render(request, 'core/listar_proveedor.html',datos)
+
+def form_del_proveedor(request, id):
+
+    proveedor = Proveedores.objects.get(idProveedor=id)
+
+    proveedor.delete()
+    
+    return redirect(to="home")
+
+def form_mod_proveedor(request, id):
+
+    proveedor = Proveedores.objects.get(idProveedor=id)
+
+    datos = {
+        'form' : ProveedoresForm(instance=proveedor)
+    }
+
+    if request.method == 'POST':
+        formulariomod = ProveedoresForm(request.POST, instance=proveedor)
+        if formulariomod.is_valid:
+            formulariomod.save()
+            datos['form'] = formulariomod
+            datos['mensaje'] = "Modificados correctamente"
+
+    return render(request, 'core/form_mod_proveedor.html', datos)
 
