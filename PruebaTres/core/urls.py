3efe7fe62dd339c5fd27b1f0ebf54f769a640d32
Sebastian Miranda <SebaMSH>
2021-06-22 22:00:14 -0400

    Migracion a Django

--- PruebaTres/core/urls.py
+++ PruebaTres/core/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import index
+from .views import index , dulce, salada, mixta, contacto, nosotros, sismos, home
+
 
 urlpatterns = [
     path('', index, name="index"),
-    
-    
-
+    path('dulce', dulce, name="dulce"),
+    path('salada', salada, name="salada"),
+    path('mixta', mixta, name="mixta"),
+    path('contacto', contacto, name="contacto"),
+    path('nosotros', nosotros, name="nosotros"),
+    path('sismos', sismos, name="sismos"),
+    path('home', home, name="home"),
 ]
